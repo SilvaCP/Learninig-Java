aa755775ec518db82efdfa58483903d65a6ce140
Carlos <79268743+SilvaCP@users.noreply.github.com>
2022-12-09 17:21:56 -0300

    Update Module08_Exercise01.java

--- Learning-Java/br/com/carlos/learningJava/Module08_Exercise01.java
+++ Learning-Java/br/com/carlos/learningJava/Module08_Exercise01.java
@@ -6,6 +6,7 @@ package br.com.carlos.learningJava;
 public class Module08_Exercise01 {
 	public static void main(String[] args) {
 		System.out.println("Teste");
+		System.out.println("Deu certo. Graças a Deus!");
 	}
 
 }
