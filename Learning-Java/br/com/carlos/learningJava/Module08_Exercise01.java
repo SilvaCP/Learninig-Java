833c07e7bb0d77f092f6a42209b5b86a36763247
Carlos <pereiracarlos1047@gmail.com>
2022-12-09 17:18:53 -0300

    First Commit

---
+++ Learning-Java/br/com/carlos/learningJava/Module08_Exercise01.java
@@ -0,0 +1,11 @@
+/**
+ * @author carlos
+ * */
+package br.com.carlos.learningJava;
+
+public class Module08_Exercise01 {
+	public static void main(String[] args) {
+		System.out.println("Teste");
+	}
+
+}
