77973709940d535d348f5061c2617bb570f3b5d8
Carlos <pereiracarlos1047@gmail.com>
2022-12-10 10:11:46 -0300

    New Commit

--- Learning-Java/br/com/carlos/learningJava/Module08_Exercise01.java
+++ Learning-Java/br/com/carlos/learningJava/Module08_Exercise01.java
@@ -1,12 +1,41 @@
 /**
  * @author carlos
+ * Operadores lógicos com if else.
  * */
 package br.com.carlos.learningJava;
 
 public class Module08_Exercise01 {
 	public static void main(String[] args) {
-		System.out.println("Teste");
-		System.out.println("Deu certo. Graças a Deus!");
+
+		// Example 1
+//		double media = 70; 
+//		String nome = "Carol";
+//		if (media >= 70 && nome.equals("Carola")) {
+//			System.out.println("Aprovado!");
+//		} else if (media < 70) {
+//			System.out.println("Reprovado!");
+//		}else {
+//			System.out.println("Não encontrado!");
+//		}
+
+		// Example 2
+
+		double nota1 = 10;
+		double nota2 = 60;
+		double nota3 = 10;
+		double nota4 = 10;
+		double media = 0;
+
+		media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+		if (media >= 70) {
+			System.out.println("Aprovado! " + media);
+		}
+		if (media >= 40 && media <= 69) {
+			System.out.println("Recuperação! " + media);
+		} else {
+			System.out.println("Reprovado! " + media);
+		}
 	}
 
 }
